c4d31f081e9cb9de8205f505218b74676bd3592c
peternguyen12346 <141355865+peternguyen12346@users.noreply.github.com>
2025-10-12 12:15:06 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-set -o errexit
+# Ngừng script nếu có lỗi
+set -o errexit  
+
+# Chạy migrate (tạo/cập nhật bảng trong database)
+python manage.py migrate
+
+# Gom toàn bộ file static (CSS, JS, images...) vào thư mục STATIC_ROOT
+python manage.py collectstatic --noinput
+
+# Khởi động Django server bằng gunicorn (Render yêu cầu chạy theo cổng môi trường PORT)
+gunicorn your_project_name.wsgi:application --bind 0.0.0.0:$PORT
