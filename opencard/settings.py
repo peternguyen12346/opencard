6ab3bc887cfbefeb95e1b8ed884d1a6c67a1ce55
unknown <C:\Users\hguyenky\AppData\Roaming\The Bat!>
2025-10-16 08:35:07 +0700

    Cập nhật Pi SDK và sửa lỗi postMessage

--- opencard/settings.py
+++ opencard/settings.py
@@ -29,7 +29,8 @@ MIDDLEWARE = [
 'django.middleware.csrf.CsrfViewMiddleware',
 'django.contrib.auth.middleware.AuthenticationMiddleware',
 'django.contrib.messages.middleware.MessageMiddleware',
-'django.middleware.clickjacking.XFrameOptionsMiddleware',
+'corsheaders.middleware.CorsMiddleware'
+# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 MIDDLEWARE = ["corsheaders.middleware.CorsMiddleware"] + MIDDLEWARE
 
